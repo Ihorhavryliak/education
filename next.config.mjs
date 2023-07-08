12aecb2ceb0bdc583a974801b2a77a0475eff735
Ihor Havryliak <94486546+Ihorhavryliak@users.noreply.github.com>
2023-07-08 15:09:26 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,16 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  images: {
+  /* images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
       },
     ],
-  },
+  }, */
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
